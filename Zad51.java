f4d34e3f1814935444728ec9295af5161b00d4f9
Marek <50408865+marcioq00@users.noreply.github.com>
2024-05-16 20:22:11 +0200

    Zad 5.1 Punkty zbioru w kolejności leksykograficznej

---
+++ Zad51.java
@@ -0,0 +1,43 @@
+package zad51;
+
+import java.util.*;
+
+/**
+ *
+ * @author Marek Jóskowiak
+ */
+class Wspolrzedna implements Comparable<Wspolrzedna> {
+    private int x, y;
+    public Wspolrzedna(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+     @Override
+    public int compareTo(Wspolrzedna other) {
+        if (this.x != other.x) {
+            return Integer.compare(this.x, other.x);
+        } else {
+            return Integer.compare(this.y, other.y);
+        }
+    }
+}
+public class Zad51 {
+    private static void wypiszElementy(TreeSet zbior) {
+        Iterator it = zbior.iterator();
+            while (it.hasNext()) {
+            System.out.println((it.next()).toString());
+        }
+    }
+    public static void main(String[] args) {
+        TreeSet<Wspolrzedna> zbior = new TreeSet<>();
+        zbior.add( new Wspolrzedna(2, 3) );
+        zbior.add( new Wspolrzedna(-3, 0) );
+        zbior.add( new Wspolrzedna(-1, 2) );
+        zbior.add( new Wspolrzedna(-1, 2) ); //duplikat nie zostanie dodany
+        zbior.add( new Wspolrzedna(-3, -2) );
+        wypiszElementy(zbior);
+    }
+}
